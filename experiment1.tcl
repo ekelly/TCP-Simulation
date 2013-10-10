6261afec35f8dcfdbb38a2f21cb124317a92ba74
dfickl <dfickl@monkeymolepanic.ccs.neu.edu>
2013-10-10 18:35:20 -0400

    Initial add experiment1.tcl

---
+++ experiment1.tcl
@@ -0,0 +1,84 @@
+#Create a simulator object
+set ns [new Simulator]
+
+#Define different colors for data flows (for NAM)
+$ns color 1 Blue
+$ns color 2 Red
+
+#Open the NAM trace file
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+#Define a 'finish' procedure
+proc finish {} {
+        global ns nf
+        $ns flush-trace
+        #Close the NAM trace file
+        close $nf
+        #Execute NAM on the trace file
+        exec nam out.nam &
+        exit 0
+}
+
+#Create six nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+#Create links between the nodes
+$ns duplex-link $n0 $n1 10Mb 0ms DropTail
+$ns duplex-link $n1 $n2 10Mb 0ms DropTail
+$ns duplex-link $n2 $n3 10Mb 0ms DropTail
+$ns duplex-link $n4 $n1 10Mb 0ms DropTail
+$ns duplex-link $n5 $n2 10Mb 0ms DropTail
+
+#Set Queue Size of link (n2-n3) to 10
+$ns queue-limit $n2 $n3 10
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n1 $n2 orient right-up
+$ns duplex-link-op $n2 $n3 orient right
+
+#Monitor the queue for link (n2-n3). (for NAM)
+$ns duplex-link-op $n2 $n3 queuePos 0.5
+
+# Setup N2
+set udp0 [new Agent/UDP]
+$ns attach-agent $n1 $udp0
+# Attach CBR to UDP connection on N2
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 attach-agent $udp0
+$cbr0 set type_ CBR
+$cbr0 set packet_size_ 1000
+$cbr0 set rate_ 1mb
+$cbr0 set random_ false
+
+# Setup N3
+set null0 [new Agent/Null]
+$ns attach-agent $n2 $null0
+
+# Connect N2 to N3
+$ns connect $udp0 $null0
+$udp0 set fid_ 2
+
+#Schedule events for the CBR and FTP agents
+$ns at 0.0 "$cbr start"
+$ns at 4.9 "$cbr stop"
+
+#Detach tcp and sink agents (not really necessary)
+$ns at 5.0 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
+
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 5.1 "finish"
+
+#Print CBR packet size and interval
+puts "CBR packet size = [$cbr set packet_size_]"
+puts "CBR interval = [$cbr set interval_]"
+
+#Run the simulation
+$ns run
+
