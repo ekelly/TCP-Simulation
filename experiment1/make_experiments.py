e9f45b4f761d0bcefdcb70d5280957fb52e1107d
Dick Fickling <dick@fickling.us>
2013-10-24 19:38:13 +0000

    Add throughputbycbr.py

--- experiment1/make_experiments.py
+++ experiment1/make_experiments.py
@@ -14,10 +14,10 @@ parser.add_option("-i", "--increment", dest="inc", type="int", default=1,
 (options, args) = parser.parse_args()
 
 settings = """
-set udprate %(mb)smb
+set udprate %(kb)skb
 
 #Open the NAM trace file
-set nf [open %(ttype)s-%(mb)s.tr w]
+set nf [open %(ttype)s-%(kb)s.tr w]
 
 #Create a simulator object
 set ns [new Simulator]
@@ -32,10 +32,11 @@ experiments = []
 
 if options.start and options.end and options.inc:
     for i in range(options.start, options.end + options.inc, options.inc):
+        i = i*100
         for tcptype in ["TCP", "TCP/Reno", "TCP/Newreno", "TCP/Vegas"]:
             ttype = "tahoe" if tcptype.find("/") == -1 else tcptype.split("/")[1].lower()
             with open("experiment%s-%s.tcl" % (i, ttype), "a+") as experiment:
-                experiment.write(settings % {"mb": i, "tcp": tcptype, "ttype": ttype})
+                experiment.write(settings % {"kb": i, "tcp": tcptype, "ttype": ttype})
                 with open("template.tcl") as template:
                     for line in template:
                         experiment.write(line)
@@ -46,7 +47,7 @@ else:
     parser.print_help()
 
 for experiment in experiments:
-    os.system("/course/cs4700f12/ns-allinone-2.35/bin/ns %s" % experiment)
+    os.system("ns %s" % experiment)
     os.remove(experiment)
 
 print ""
---
+++ experiment1/throughputbycbr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import fileinput
+from collections import Counter,defaultdict
+
+def main():
+    cbrs = defaultdict(Counter)
+    for tcp_variant in ['reno', 'tahoe', 'newreno', 'vegas']:
+        print tcp_variant
+        # read $tcp_variant-*.tr, count number of packets acked / number of seconds
+        # 0.0   123
+        # 0.1   102
+        # ...   ...
+        # write to tcp_variant-throughput.txt
+        for cbr in range(100,1100,100):
+            with open("%s-%s.tr" % (tcp_variant, cbr)) as in_file:
+                for line in map(lambda line: line.split(" "), in_file):
+                    if len(line) and line[0] == 'r' and line[4] == 'tcp':
+                        cbrs[cbr][tcp_variant] += 1
+
+    print 'cbr\t'+'\t'.join(sorted(cbrs.values()[0].keys()))
+    for cbr,variants in sorted(cbrs.items(), key=lambda item: int(item[0])):
+        row = "%s" % cbr
+        for variant,count in sorted(variants.items(), key=lambda item: item[0]):
+            row += "\t%s" % count
+        print row
+
+if __name__ == "__main__":
+    main()
