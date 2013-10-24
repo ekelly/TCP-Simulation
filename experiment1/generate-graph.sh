264776e091f5ce40e54c2ff1cf586663aaa66f94
Eric Kelly <me@useric.com>
2013-10-24 11:00:55 -0400

    Fixed minor generate graph bug

--- experiment1/generate-graph.sh
+++ experiment1/generate-graph.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat $1 | python throughput.py > throughput.txt && gnuplot < throughput.plot && gnome-open throughput.png
+cat $1 | python throughput.py > throughput.txt && gnuplot < throughput.plot && mv throughput.png $1.png && gnome-open $1.png
